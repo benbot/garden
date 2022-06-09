122d6ae7a1e59573aca9835365c28ba3d6e25121
benbot <bbotwin@atlassian.com>
2022-06-09 10:22:29 -0400

    to railway

--- src/plant.rs
+++ src/plant.rs
@@ -11,7 +11,7 @@ struct SpawnTime(f64);
 struct Growing(bool);
 
 #[derive(Component)]
-struct Plant;
+struct Plant(PlantType);
 
 #[derive(Bundle)]
 struct PlantBundle {
@@ -23,46 +23,59 @@ struct PlantBundle {
     token: TokenBundle,
 }
 
+pub enum PlantType {
+    WHEAT,
+}
+
 impl PlantBundle {
     fn new(x: i32, y: i32, time: &Time) -> Self {
         Self {
             spawn_time: SpawnTime(time.seconds_since_startup()),
             growing: Growing(true),
-            plant: Plant,
+            plant: Plant(PlantType::WHEAT),
             token: TokenBundle::new(x, y, 'P'),
         }
     }
 }
 
-// TODO: Probably want to call this grow_wheat, since it makes pretty good looking wheat
 fn grow(
     mut cmd: Commands,
-    mut q: Query<(&mut Token, &Position, &mut SpawnTime, &mut Growing), With<Plant>>,
+    mut q: Query<(&mut Token, &Position, &mut SpawnTime, &mut Growing, &Plant)>,
     time: Res<Time>,
 ) {
-    for (mut p, pos, mut spawn_time, mut growing) in q.iter_mut() {
-        if time.seconds_since_startup() - spawn_time.0 > 3.0 {
-            match p.0 {
-                'P' => p.0 = '|',
-                '\\' => {}
-                '/' => {}
-                '|' => {
-                    p.0 = '|';
-                    let u: i32 = thread_rng().gen_range(-1..=1);
-                    let mut plant = PlantBundle::new(pos.0 + u, pos.1 + 1, time.as_ref());
-                    plant.token.token = match u {
-                        0 => Token('|'),
-                        1 => Token('/'),
-                        -1 => Token('\\'),
-                        _ => panic!("woah {} not expected!", u),
-                    };
-                    cmd.spawn_bundle(plant);
+    for (mut p, pos, mut spawn_time, mut growing, Plant(plant_type)) in q.iter_mut() {
+        if !growing.0 {
+            continue;
+        }
+
+        match plant_type {
+            PlantType::WHEAT => {
+                if time.seconds_since_startup() - spawn_time.0 > 3.0
+                    && thread_rng().gen_range(0..100) > 30
+                {
+                    match p.0 {
+                        'P' => p.0 = '|',
+                        '\\' => {}
+                        '/' => {}
+                        '|' => {
+                            p.0 = '|';
+                            let u: i32 = thread_rng().gen_range(-1..=1);
+                            let mut plant = PlantBundle::new(pos.0 + u, pos.1 + 1, time.as_ref());
+                            plant.token.token = match u {
+                                0 => Token('|'),
+                                1 => Token('/'),
+                                -1 => Token('\\'),
+                                _ => panic!("woah {} not expected!", u),
+                            };
+                            cmd.spawn_bundle(plant);
+                        }
+                        _ => panic!("Woah {} not expected!", p.0),
+                    }
+                    spawn_time.0 = time.seconds_since_startup();
+                    if thread_rng().gen_range(0..100) as u32 > 90 {
+                        growing.0 = false;
+                    }
                 }
-                _ => panic!("Woah {} not expected!", p.0),
-            }
-            spawn_time.0 = time.seconds_since_startup();
-            if thread_rng().gen_range(0..100) as u32 > 90 {
-                growing.0 = false;
             }
         }
     }
